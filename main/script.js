8bfb7bf5f92c5fd4044635239d9818c98bb3a3a2
Peziiim <eduardoorx@gmail.com>
2025-05-29 14:59:02 -0300

    feat: login auth

--- main/script.js
+++ main/script.js
@@ -6,7 +6,6 @@ const port = 3000;
 const anoAtual = new Date().getFullYear()
 
 app.set('view engine', 'ejs')
-
 app.set('views', 'views')
 
 app.use(express.static('public'))
@@ -18,6 +17,29 @@ app.get("/", (req, res) => {
 }); 
 
 
+app.post("/submit", (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  let emailError = "";
+  let passError = "";
+
+ if (!email.includes("gmail")) {
+    emailError = "Email inválido";
+  }
+  if (password.length < 7) {
+    passError = "Tamanho mínimo de 7 caracteres";
+  }
+
+  if (emailError || passError) {
+    res.render('index', { anoAtual, emailError, passError });
+  } else {
+    res.redirect('home');
+  }
+})
+
+app.get("/login", (req, res) => {
+    res.render('login', {anoAtual})
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
